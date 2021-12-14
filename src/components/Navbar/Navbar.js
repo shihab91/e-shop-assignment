a7126c791f872fd72c0e2448ba0cd79d24e1cddd
shihab <shumonshihab62@gmail.com>
2021-12-14 22:31:20 +0600

    navigation done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,33 +5,36 @@ import { BsCart2, BsPerson, BsSearch } from "react-icons/bs";
 import "./Navbar.css";
 const Navbar = () => {
   return (
-    <Container>
-      <Row xs={1} md={3} className="navigation-bar">
-        <Col>
-          <img src={logo} alt="the logo" width="80px" />
-        </Col>
-        <Col className="header-links">
-          <ul>
-            <li>Men</li>
-            <li>Women</li>
-            <li>Kids</li>
-          </ul>
-        </Col>
-        <Col className="header-icons">
-          <ul>
-            <li>
-              <BsCart2 />
-            </li>
-            <li>
-              <BsPerson />
-            </li>
-            <li>
-              <BsSearch />
-            </li>
-          </ul>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Container>
+        <Row xs={1} md={3} className="navigation-bar">
+          <Col className="w-fit-content">
+            <img src={logo} alt="the logo" width="80px" className="logo" />
+          </Col>
+          <Col className="my-auto p-0">
+            <ul className="header-links">
+              <li>Men</li>
+              <li>Women</li>
+              <li>Kids</li>
+            </ul>
+          </Col>
+          <Col className=" my-auto ">
+            <ul className="header-icons float-md-end">
+              <li>
+                <BsCart2 />
+              </li>
+              <li>
+                <BsPerson />
+              </li>
+              <li>
+                <BsSearch />
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+      <hr className="horizontal-line" />
+    </>
   );
 };
 
