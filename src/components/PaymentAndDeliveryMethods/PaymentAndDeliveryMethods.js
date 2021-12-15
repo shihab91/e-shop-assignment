14399277eb79a5ba896c603122a55404e7e31381
shihab <shumonshihab62@gmail.com>
2021-12-15 11:52:45 +0600

    payment and dilevery methods desing done

--- src/components/PaymentAndDeliveryMethods/PaymentAndDeliveryMethods.js
+++ src/components/PaymentAndDeliveryMethods/PaymentAndDeliveryMethods.js
@@ -2,22 +2,25 @@ import React, { useEffect, useState } from "react";
 import "./PaymentAndDeliveryMethods.css";
 import useMethods from "../../hooks/useMethods";
 const PaymentAndDeliveryMethods = () => {
-  // const [methods, setMethods] = useState([]);
-  // useEffect(() => {
-  //   fetch("./paymentAndDeliveryMethods.json")
-  //     .then((res) => res.json())
-  //     .then((data) => setMethods(data));
-  // }, []);
-  // // return { methods };
   const { methods } = useMethods();
   console.log(methods);
   return (
-    <div>
-      <h6>Payment Methods</h6>
-      <div className="payment-methods d-flex flex-wrap">
-        {methods.map((method) => (
-          <img src={method.image} alt="" />
-        ))}
+    <div className="payment-shipping-method-container">
+      <div>
+        <h6>Payment Methods</h6>
+        <div className="payment-methods d-flex flex-wrap">
+          {methods.slice(0, 6).map((method) => (
+            <img src={method.image} alt="" />
+          ))}
+        </div>
+      </div>
+      <div className="mt-5">
+        <h6>Delivery Methods</h6>
+        <div className="delivery-methods mt-3 d-flex flex-wrap">
+          {methods.slice(6, 10).map((method) => (
+            <img src={method.image} alt="" />
+          ))}
+        </div>
       </div>
     </div>
   );
